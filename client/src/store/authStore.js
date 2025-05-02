310d6fc15272ab4873d42b9b31a2ee72b1edfd27
itsjhaydev <earlm643@gmail.com>
2025-05-02 22:52:12 +0800

    removed backend folder

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development"
-	? "http://localhost:5000/api"
-	: "/api";
+const BASE_URL =
+	import.meta.env.MODE === "development"
+		? "http://localhost:5000/api"
+		: import.meta.env.VITE_API_URL || "/api";
+
 
 const AUTH_API = `${BASE_URL}/auth`;
 const RESERVATION_API = `${BASE_URL}/reservations`;
