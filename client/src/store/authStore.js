13c6f11b4fa2e8ed78e9b34aa625b0c0285ca650
itsjhaydev <earlm643@gmail.com>
2025-05-03 19:35:22 +0800

    update checkAuth

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -73,12 +73,15 @@ export const useAuthStore = create((set) => ({
 	checkAuth: async () => {
 		set({ isCheckingAuth: true, error: null });
 		try {
-			const response = await axios.get(`${AUTH_API}/check-auth`);
+			const response = await axios.get(`${AUTH_API}/check-auth`, {
+				withCredentials: true // 🔥 REQUIRED
+			});
 			set({ user: response.data.user, isAuthenticated: true, isCheckingAuth: false });
 		} catch (error) {
 			set({ error: null, isCheckingAuth: false, isAuthenticated: false });
 		}
 	},
+	
 
 	forgotPassword: async (email) => {
 		set({ isLoading: true, error: null });
